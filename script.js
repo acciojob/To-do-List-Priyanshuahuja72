58699fb13f59000c1fb134f589556c8610e5b2af
Priyanshu Ahuja <85331994+Priyanshuahuja72@users.noreply.github.com>
2023-02-04 13:35:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,17 @@
 let button = document.getElementById("addTodoBtn");
 button.addEventListener('click' , function(){
 	let name = document.getElementById('newTodoInput').value;
+	if(name == "")
+	{
+		error.innerHTML = "string should not be empty";
+     }
+	else
+	{
 	let box = document.getElementById('todolist');
 	let li = document.createElement('li');
 	li.textContent = name;
 	box.appendChild(li);
 	document.getElementById('newTodoInput').value = " ";
+	}
+	
 })
