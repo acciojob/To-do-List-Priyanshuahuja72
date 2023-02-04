371ddae5eb86cfc7aaf96fea52f846221bd7737a
Priyanshu Ahuja <85331994+Priyanshuahuja72@users.noreply.github.com>
2023-02-04 13:21:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here
-
+let button = document.getElementById("addTodoBtn");
+button.addEventListener('click' , function(){
+	let name = document.getElementById('newTodoInput').value;
+	let box = document.getElementById('todolist');
+	let li = document.createElement('li');
+	li.textContent = name;
+	box.appendChild(li);
+	document.getElementById('newTodoInput').value = " ";
+})
